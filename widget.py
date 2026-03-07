99efcc0b1d80559916f78f88971691bf0206b923
lilfetz22 <billfetzner@yahoo.com>
2026-03-07 14:59:49 -0500

    feat: add dashboard widget rendering
    Closes: #3

--- widget.py
+++ widget.py
@@ -6,3 +6,6 @@ class Widget:
 
     def render(self) -> str:
         return f"<widget>{self.name}</widget>"
+
+    def render_dashboard(self) -> str:
+        return f"<dashboard><widget>{self.name}</widget></dashboard>"
